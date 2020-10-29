3652018c3cd7c2923fe2f6870000fee832fef714
jtaylor6 <jarrede20@gmail.com>
2020-10-29 17:38:54 -0400

    completed the webscrape of the comment-bubble

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-axios.get('https://www.forextradingbig.com/instaforex-broker-review/') 
-    .then(response => 
-            {const html = response.data; })
-
-const $ = cheerio.load(html);
-
-const scrapedata = $('a', '.comment-bubble').text(); 
-    console.log(scrapedata);
+axios.get('https://www.forextradingbig.com/instaforex-broker-review/')
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const scrapedata = $('a', '.comment-bubble').text();;
+        console.log(scrapedata);
+    }).catch(error => {
+        console.log(error);
+    });
