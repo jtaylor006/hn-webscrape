f65bfcc93ef9dcc756acea4cf6ac67bf67cdfee3
jtaylor6 <jarrede20@gmail.com>
2020-10-28 23:10:20 -0400

    installed packagaes needed for webscraping

---
+++ index.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+axios.get('https://www.forextradingbig.com/instaforex-broker-review/') 
+    .then(response => 
+            {const html = response.data; })
+
+const $ = cheerio.load(html);
+
+const scrapedata = $('a', '.comment-bubble').text(); 
+    console.log(scrapedata);
